d4e4dcf9095b05a52fddd772726b7d42dc68cc7a
Pantelis Giazitsis <giazitsispantelis@gmail.com>
2018-02-04 21:41:11 +0200

    picker functionality

--- Currency Calculator/Controllers/CurrencyPickerViewController.swift
+++ Currency Calculator/Controllers/CurrencyPickerViewController.swift
@@ -8,12 +8,23 @@
 
 import UIKit
 
-class CurrencyPickerViewController: UIViewController {
+class CurrencyPickerViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    @IBOutlet weak var viewHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var pickerView: UIPickerView!
+    @IBOutlet weak var doneButton: UIButton!
+    @IBOutlet weak var cancelButton: UIButton!
+    
+    var currencies: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        
+        viewHeightConstraint.constant = UIScreen.main.bounds.size.height/2.0
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +32,26 @@ class CurrencyPickerViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // MARK: Actions
+    
+    @IBAction func doneButtonTapped(_ sender: Any) {
     }
-    */
+    
+    @IBAction func cancelButtonTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+ 
+    // MARK: UIPickerViewDelegate, UIPickerViewDataSource
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return currencies.count
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
 
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return currencies[row]
+    }
 }
