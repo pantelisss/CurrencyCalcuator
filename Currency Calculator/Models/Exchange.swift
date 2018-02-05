bf87f4aad9373d2819614342bce151c30c67ebd0
Pantelis Giazitsis <giazitsispantelis@gmail.com>
2018-02-05 11:08:12 +0200

    added exchange model

---
+++ Currency Calculator/Models/Exchange.swift
@@ -0,0 +1,33 @@
+//
+//  Exchange.swift
+//  Currency Calculator
+//
+//  Created by Pantelis Giazitsis on 05/02/2018.
+//  Copyright © 2018 Pantelis Giazitsis. All rights reserved.
+//
+
+import Foundation
+
+struct Exchange {
+    let base: String
+    let date: String
+    let rates: [String : Float]
+}
+
+extension Exchange {
+    init?(dict: [String : Any]) {
+        guard let base = dict["base"] as? String else {return nil}
+        guard let date = dict["date"] as? String else {return nil}
+        guard let rates = dict["rates"] as? [String : Float] else {return nil}
+        
+        self.base = base
+        self.date = date
+        self.rates = rates
+    }
+    
+    func getPrice(currency: String, amount: Float) -> Float {
+        guard let price = rates[currency] else {return 0.0}
+        
+        return price * amount
+    }
+}
